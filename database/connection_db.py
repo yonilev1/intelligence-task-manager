af151701a9b8df03c1f0fbcf12cd6b92c7c9d6b4
Yoni <yonilev2021@gmail.com>
2026-06-17 12:21:22 +0300

    implement create_table in Dbconnection and tested

--- database/connection_db.py
+++ database/connection_db.py
@@ -2,6 +2,9 @@ import mysql.connector
 
 class DbConnection:
     def get_connection(self):
+        """
+        return the connection
+        """
         return mysql.connector.connect(
             user='root',
             password='1234',
@@ -11,13 +14,40 @@ class DbConnection:
     
 
     def create_database(self):
+        """
+        connect and create db if does not exist already
+        """
         conn = self.get_connection()
         cursor = conn.cursor()
         cursor.execute("""
         CREATE DATABASE IF NOT EXISTS Intelligence_db;
         """)
 
+
+    def create_tables(self):
+        """
+        connect and create db if does not exist already,
+        then use db and create table if does not exist already
+        """
+        self.create_database()
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute("USE Intelligence_db")
+        cursor.close()
+
+        cursor = conn.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS agents (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            specialty VARCHAR(50) NOT NULL,
+            is_active BOOLEAN NOT NULL DEFAULT TRUE,
+            completed_missions INT NOT NULL DEFAULT 0, 
+            failed_missions INT NOT NULL DEFAULT 0,             
+            agent_rank ENUM('Junior', 'Senior', 'Commander') NOT NULL);
+        """)
+
     
 conn = DbConnection()
-cr = conn.create_database()
+cr = conn.create_tables()
 print(cr)
